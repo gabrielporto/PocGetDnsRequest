dbc96716ab2a640db1083f2fa60670ab33b1078f
Gabriel Porto Lopes <gabriel.lopes@nava.com.br>
2022-08-02 22:55:23 -0300

    Add project files.

---
+++ PocGetDnsRequest/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PocGetDnsRequest.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { Newtonsoft.Json.JsonConvert.SerializeObject(Request.Host) };
+        }
+    }
+}
